889e60b36e989df6298b0001e712f463b5ca51e0
Shivani-012 <63118364+Shivani-012@users.noreply.github.com>
2022-02-17 19:24:05 -0500

    Final changes and bug updates.

--- app/src/main/java/com/example/map524_cashregister/MyApp.java
+++ app/src/main/java/com/example/map524_cashregister/MyApp.java
@@ -5,12 +5,11 @@ import android.app.Application;
 public class MyApp extends Application {
 
     // declare and initialize default products for app
-    Product pants = new Product("Shirt", 35, 19.99);
-    Product shoes = new Product("Belt", 100, 7.50);
-    Product hats = new Product("Sweater", 23, 24.99);
+    Product belt = new Product("Belt", 100, 7.50);
+    Product sweater = new Product("Sweater", 23, 24.99);
     Product jeans = new Product("Jeans", 40, 59.99);
 
-    Product[] products = {pants, shoes, hats, jeans}; // create an array of products
+    Product[] products = {belt, sweater, jeans}; // create an array of products
 
     // declare and initialize productManager with array of products
     ProductManager productList = new ProductManager(products);
